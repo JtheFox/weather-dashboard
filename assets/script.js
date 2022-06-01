939ee2ffc0bd4518fc8aa70a5d6605d09b2f4197
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-06-01 15:10:19 -0400

    Added forecast card display from weather data

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const apiCall = dummydata;
 const apiKey = '';
 const weatherCities = ['New York City', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia', 'San Antonio', 'San Diego']
+//TODO: Set up assigned icons for weather conditions
 
 $(function() {
     // add predefined cities to city search buttons
@@ -63,7 +64,7 @@ $(function() {
         }
         for (let day of forecast) {
             weatherData.forecast.push({
-                date: moment(day.dt),
+                date: moment.unix(day.dt).format("M/D/YYYY"),
                 cond: day.weather[0].main,
                 temp: day.temp.day,
                 wind: day.wind_speed,
@@ -81,10 +82,19 @@ const displayWeather = (data) => {
     // display current weather values from api response
     $('.city').text(`${data.location.city}, ${data.location.country}`);
     $('.currDate').text(`(${data.current.date})`);
-    //TODO: Set up assigned icons for weather conditions
     $('.currWeatherIcon').attr('src', ``).attr('alt', ``);
     $('.currTemp').text(`${data.current.temp} °F`);
     $('.currWind').text(`${data.current.wind} mph`);
     $('.currHumidity').text(`${data.current.humidity}%`);
     $('.currUV').text(data.current.uv);
+
+    // display forecast weather on cards
+    $('.forecastCard').each((i, card) => {
+        let forecastData = data.forecast[i];
+        $(card).find('.forecastDate').text(forecastData.date);
+        $(card).find('.forecastImg').attr('src', ``).attr('alt', ``);
+        $(card).find('.forecastTemp').text(forecastData.temp);
+        $(card).find('.forecastWind').text(forecastData.wind);
+        $(card).find('.forecastHumidity').text(forecastData.humidity);
+    })
 }
